daf75791ef23627347af77656c3c145ea6f1ed7b
AntohaDontLookAtMe <powerwolfgdansk@gmail.com>
2025-04-19 20:06:00 +0400

    1
    1

---
+++ Task.CSharpBase/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Class
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Это ожегов алексей !");
+        //Console.ReadKey();
+        Console.ReadLine();
+    }
+}
+
